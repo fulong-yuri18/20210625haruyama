0a33f9186e6b7da2f5924bdf960a7da9df0171a6
haruyama <fulong.yuri18@gmail.com>
2021-06-24 01:44:07 +0900

    css修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>COACHTECH</title>
-  <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">
-  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('/css/reset.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('/css/style.css') }}">
 </head>
 
 <body>
